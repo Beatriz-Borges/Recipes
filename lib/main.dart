37f3c88c7a0ea05f7fcdc87e640634dbb076e1c8
Beatriz <beatrizsobsantana@gmail.com>
2024-02-27 03:51:22 -0300

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/data/dummy_data.dart';
+import 'package:recipes/models/meal.dart';
+import 'package:recipes/models/settings.dart';
+import 'package:recipes/screens/categories_meals_screen.dart';
+import 'package:recipes/screens/meal_detail.dart';
+import 'package:recipes/screens/settings_screen.dart';
+import 'package:recipes/screens/tabs_screen.dart';
+import 'package:recipes/utils/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
+  List<Meal> _availableMeals = dummyMeals;
+  final List<Meal> _favoriteMeals = [];
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+      _availableMeals = dummyMeals.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+        return !filterGluten && !filterLactose && !filterVegan && !filterVegetarian;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal) ? _favoriteMeals.remove(meal) : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos cozinhar?',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromARGB(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      routes: {
+        AppRoutes.home: (ctx) => TabsScreen(_favoriteMeals),
+        AppRoutes.settings: (ctx) => SettingsScreen(settings, _filterMeals),
+        AppRoutes.categoriesMeals: (context) => CategoriesMealsScreen(_availableMeals),
+        AppRoutes.mealDetail: (context) => MealDetailScreen(_toggleFavorite, _isFavorite),
+      },
+    );
+  }
+}
---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/components/meal_item.dart';
+import 'package:recipes/models/meal.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen(this.favoriteMeals, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Nenhuma refeição foi marcada como favoritas'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (context, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
+  }
+}
